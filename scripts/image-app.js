b1528672a7f9090227b032097b23befb483a8567
sonali <sonalikatara@gmail.com>
2016-07-18 10:22:48 -0700

    add web-worker image-app.js to improve speed

--- scripts/image-app.js
+++ scripts/image-app.js
@@ -6,6 +6,8 @@
   var canvas = document.querySelector('#image');
   var ctx = canvas.getContext('2d');
 
+  var theWorker = new Worker("scripts/worker.js");
+
   function handleImage(e){
     var reader = new FileReader();
     reader.onload = function(event){
@@ -40,23 +42,39 @@
 
     toggleButtonsAbledness();
 
+
+    theWorker.postMessage({'imageData': imageData,'type': type});
+    theWorker.onmessage = function(e){
+      toggleButtonsAbledness();
+      var image = e.data;
+      if(image){
+        return ctx.putImageData(e.data, 0, 0);
+      }
+
+    } ;
     // Hint! This is where you should post messages to the web worker and
     // receive messages from the web worker.
 
-    length = imageData.data.length / 4;
+ /*   length = imageData.data.length / 4;
     for (i = j = 0, ref = length; 0 <= ref ? j <= ref : j >= ref; i = 0 <= ref ? ++j : --j) {
       r = imageData.data[i * 4 + 0];
       g = imageData.data[i * 4 + 1];
       b = imageData.data[i * 4 + 2];
       a = imageData.data[i * 4 + 3];
+
       pixel = manipulate(type, r, g, b, a);
+
       imageData.data[i * 4 + 0] = pixel[0];
       imageData.data[i * 4 + 1] = pixel[1];
       imageData.data[i * 4 + 2] = pixel[2];
       imageData.data[i * 4 + 3] = pixel[3];
     }
-    toggleButtonsAbledness();
-    return ctx.putImageData(imageData, 0, 0);
+
+      toggleButtonsAbledness();
+      return ctx.putImageData(imageData, 0, 0);
+    */
+
+
   };
 
   function revertImage() {
